622a740bacadb4b8cc42be469522f517cadba27e
BlazingTwist <m.h.Freedom2@web.de>
2021-03-16 11:00:32 +0100

    260 / 284 remaining

---
+++ src/main/java/blazingtwist/wswebservice/functions/PurchaseItems.java
@@ -0,0 +1,18 @@
+package blazingtwist.wswebservice.functions;
+
+import blazingtwist.wswebservice.WebServiceFunction;
+import blazingtwist.wswebservice.WebServiceFunctionConstructor;
+import com.sun.net.httpserver.HttpExchange;
+import java.util.Map;
+
+public class PurchaseItems extends WebServiceFunction {
+	@WebServiceFunctionConstructor
+	public PurchaseItems(String contextName) {
+		super(contextName);
+	}
+
+	@Override
+	public void handle(HttpExchange exchange, Map<String, String> params) {
+		// TODO
+	}
+}
---
+++ src/main/java/blazingtwist/wswebservice/functions/RedeemItem.java
@@ -0,0 +1,18 @@
+package blazingtwist.wswebservice.functions;
+
+import blazingtwist.wswebservice.WebServiceFunction;
+import blazingtwist.wswebservice.WebServiceFunctionConstructor;
+import com.sun.net.httpserver.HttpExchange;
+import java.util.Map;
+
+public class RedeemItem extends WebServiceFunction {
+	@WebServiceFunctionConstructor
+	public RedeemItem(String contextName) {
+		super(contextName);
+	}
+
+	@Override
+	public void handle(HttpExchange exchange, Map<String, String> params) {
+		// TODO
+	}
+}
