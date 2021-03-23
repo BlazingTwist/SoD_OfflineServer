cc58e54c401f0b11e2ae1a7592895065a30bcc8d
BlazingTwist <m.h.Freedom2@web.de>
2021-03-23 16:51:58 +0100

    236 / 284 remaining

---
+++ src/main/java/blazingtwist/wswebservice/functions/GetImageData.java
@@ -0,0 +1,18 @@
+package blazingtwist.wswebservice.functions;
+
+import blazingtwist.wswebservice.WebServiceFunction;
+import blazingtwist.wswebservice.WebServiceFunctionConstructor;
+import com.sun.net.httpserver.HttpExchange;
+import java.util.Map;
+
+public class GetImageData extends WebServiceFunction {
+	@WebServiceFunctionConstructor
+	public GetImageData(String contextName) {
+		super(contextName);
+	}
+
+	@Override
+	public void handle(HttpExchange exchange, Map<String, String> params) {
+		// TODO
+	}
+}
